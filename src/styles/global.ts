7abfe85fd5807227bf29bbc17eb87a4d869ba666
Felipe Seabra <feliperoce@gmail.com>
2023-05-05 17:53:47 -0300

    feat: add style to global btn

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -27,6 +27,48 @@ export default createGlobalStyle`
     color: ${(props) => props.theme.colors.secundary};
   }
 
+  .global-btn {
+    position: relative;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    width: 150px;
+    height: 100%;
+    border: 2px solid ${(props) => props.theme.colors.secundary};
+    border-radius: 8px;
+    font-size: 1.2rem;
+    color: ${(props) => props.theme.colors.secundary};
+    text-decoration: none;
+    font-weight: 600;
+    letter-spacing: 0.1rem;
+    z-index: 1;
+    overflow: hidden;
+    transition: 0.5s;
+  }
+  .global-btn:hover {
+    color: ${(props) => props.theme.colors.secundary};
+    border: 2px solid ${(props) => props.theme.colors.secundary};
+  }
+  .global-btn::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 0%;
+    height: 100%;
+    background: ${(props) => props.theme.colors.primary};
+    z-index: -1;
+    transition: 0.5s;
+  }
+  .global-btn:hover::before {
+    width: 100%;
+    transition: 0.5s;
+  }
+  
+  .global-btn:disable {
+    cursor: not-allowed;
+  }
+
   #hero {
     position: absolute;
     left: 50%;
