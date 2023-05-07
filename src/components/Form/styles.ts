2465b82a9468ca9883dc2f06dc63bd9a59feb146
Felipe Seabra <feliperoce@gmail.com>
2023-05-07 17:32:54 -0300

    fix: color labels to form

--- src/components/Form/styles.ts
+++ src/components/Form/styles.ts
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   position: relative;
+  color: #101010;
+
   .form {
     display: flex;
     flex-direction: column;
