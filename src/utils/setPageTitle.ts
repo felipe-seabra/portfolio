bb0a832ac537e58dd1cf5aabe6615e05bd8af9bf
Felipe Seabra <feliperoce@gmail.com>
2023-05-21 16:23:23 -0300

    feat: create function windowScrollToTop

--- src/utils/setPageTitle.ts
+++ src/utils/setPageTitle.ts
@@ -1,3 +1,6 @@
+import { windowScrollToTop } from './windowScrollToTop';
+
 export default function setPageTitle(title: string): void {
   document.title = title;
+  windowScrollToTop();
 }
---
+++ src/utils/windowScrollToTop.ts
@@ -0,0 +1,3 @@
+export const windowScrollToTop = () => {
+  window.scrollTo(0, 0);
+};
